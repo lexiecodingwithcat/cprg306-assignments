6fb83f9254f5f26bbc6de210b3871f6d28e8730a
Tianzi Cui <cuitianzi0@gmail.com>
2024-09-09 11:39:54 -0600

    finished week-2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+export default function StudentInfo() {
+  return (
+    <div>
+      <h3>Student Information</h3>
+      <p>Name: Tianzi Cui (Lexie) 👋</p>
+      <Link href="https://github.com/lexiecodingwithcat">GitHub Link</Link>
+    </div>
+  );
+}
