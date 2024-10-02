9afa6ae848c9e6d17a0b262e8f25475b9e3b04e8
Tianzi Cui <cuitianzi0@gmail.com>
2024-10-02 13:18:11 -0600

    make quantity selector reusable

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -1,11 +1,12 @@
 "use client";
 
+import { useState } from "react";
 import QuantitySelector from "./quantitySelector";
 function NewItem() {
   const [quantity, setQuantity] = useState(1);
   return (
     <main className="flex justify-center mt-5 text-white">
-      <QuantitySelector quantity={quantity} setQuantity={setQuantity}/>
+      <QuantitySelector quantity={quantity} setQuantity={setQuantity} />
     </main>
   );
 }
